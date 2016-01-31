ccf49c32bb66c6518398b7b9205021791f346224
Fabien Batteix <skywodd@gmail.com>
2016-01-31 14:00:38 +0100

    Upgrade version 1.0.2 -> 1.0.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 # Setup config
 setup(
     name='skcodeonlinetester',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     include_package_data=True,
     license='AGPLv3',
