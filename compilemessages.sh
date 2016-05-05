f0f3135176593769b2a7bc1874cd599c56f4305b
Fabien Batteix <skywodd@gmail.com>
2016-05-05 14:34:20 +0200

    Avoid problem with path including whitespace

--- compilemessages.sh
+++ compilemessages.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-here=`pwd`
+readonly here=`pwd`
+
 echo Compile .mo in root
-python $here/manage_prod.py compilemessages
+python "$here/manage_prod.py" compilemessages
 
 for d in apps/*/ ; do
-    if [[ $d != *"__pycache__"* ]]
+    if [[ "$d" != *"__pycache__"* ]]
     then
-        echo Compile .mo in $d
-        (cd $d && python $here/manage_prod.py compilemessages)
+        echo Compile .mo in "$d"
+        (cd "$d" && python "$here/manage_prod.py" compilemessages)
     fi
 done
