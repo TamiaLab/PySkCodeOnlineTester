8933ffce5163f91d23c38051e83b293eaad6435c
Fabien Batteix <skywodd@gmail.com>
2016-05-05 15:09:19 +0200

    Update root url conf according to the new django 1.9 urls declaration scheme.

--- skcodeonlinetester/urls.py
+++ skcodeonlinetester/urls.py
@@ -4,34 +4,34 @@ Root URLCONF for the SkCodeOnlineTester project.
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.sitemaps import views as sitemaps_views
 
 
 # Patch admin site
-admin.site.site_title = 'TamiaLab admin'
+admin.site.site_title = 'TamiaLab administration'
 admin.site.site_header = 'TamiaLab administration'
 
 
 # Root URL patterns configuration
-urlpatterns = patterns('',
+urlpatterns = [
 
     # Home page
     url(r'^', include('apps.home.urls', namespace='home')),
 
     # Admin pages
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 # Sitemap index and section
 sitemaps = {
     # TODO Add all sitemaps here
 }
-urlpatterns += patterns('',
-    (r'^sitemap\.xml$', sitemaps_views.index, {'sitemaps': sitemaps}),
-    (r'^sitemap-(?P<section>.+)\.xml$', sitemaps_views.sitemap, {'sitemaps': sitemaps}),
-)
+urlpatterns += [
+    url(r'^sitemap\.xml$', sitemaps_views.index, {'sitemaps': sitemaps}),
+    url(r'^sitemap-(?P<section>.+)\.xml$', sitemaps_views.sitemap, {'sitemaps': sitemaps}),
+]
 
 # Static/media files serving for debug ONLY, static() do nothing when DEBUG=False
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
@@ -40,6 +40,6 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Debug toolbar
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
-    )
+    ]
