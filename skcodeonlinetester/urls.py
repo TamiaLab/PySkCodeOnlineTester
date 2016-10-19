6e53da044213ec5211b2133544d4be5b9d0560b0
Fabien Batteix <skywodd@gmail.com>
2016-10-19 22:33:38 +0200

    Remove useless code

--- skcodeonlinetester/urls.py
+++ skcodeonlinetester/urls.py
@@ -5,13 +5,6 @@ Root URLCONF for the SkCodeOnlineTester project.
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include, url
-from django.contrib import admin
-from django.contrib.sitemaps import views as sitemaps_views
-
-
-# Patch admin site
-admin.site.site_title = 'TamiaLab administration'
-admin.site.site_header = 'TamiaLab administration'
 
 
 # Root URL patterns configuration
@@ -19,18 +12,6 @@ urlpatterns = [
 
     # Home page
     url(r'^', include('apps.home.urls', namespace='home')),
-
-    # Admin pages
-    url(r'^admin/', include(admin.site.urls)),
-]
-
-# Sitemap index and section
-sitemaps = {
-    # TODO Add all sitemaps here
-}
-urlpatterns += [
-    url(r'^sitemap\.xml$', sitemaps_views.index, {'sitemaps': sitemaps}),
-    url(r'^sitemap-(?P<section>.+)\.xml$', sitemaps_views.sitemap, {'sitemaps': sitemaps}),
 ]
 
 # Static/media files serving for debug ONLY, static() do nothing when DEBUG=False
